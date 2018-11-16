17f16564cf8eb75ea250638b634efa0df2d2c45c
Garret Bever <gbever@graphicdesignelite.com>
2018-11-16 16:38:21 -0500

    Initial Push
    Initial Push for CDT Website

---
+++ gulpfile.js
@@ -0,0 +1,124 @@
+var gulp        = require('gulp'),
+    sass        = require('gulp-sass'),
+    rename      = require('gulp-rename'),
+    cssmin      = require('gulp-minify-css'),
+    concat      = require('gulp-concat'),
+    uglify      = require('gulp-uglify'),
+    jshint      = require('gulp-jshint'),
+    scsslint    = require('gulp-sass-lint'),
+    cache       = require('gulp-cached'),
+    prefix      = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync'),
+    reload      = browserSync.reload,
+    htmlmin     = require('gulp-htmlmin');
+    size        = require('gulp-size'),
+    imagemin    = require('gulp-imagemin'),
+    nunjucksRender = require('gulp-nunjucks-render'),
+    pngquant    = require('imagemin-pngquant'),
+    plumber     = require('gulp-plumber'),
+    deploy      = require('gulp-gh-pages'),
+    notify      = require('gulp-notify');
+    
+
+gulp.task('scss', function() {
+    var onError = function(err) {
+      notify.onError({
+          title:    "Gulp",
+          subtitle: "Failure!",
+          message:  "Error: <%= error.message %>",
+          sound:    "Beep"
+      })(err);
+      this.emit('end');
+  };
+
+  return gulp.src('scss/main.scss')
+    .pipe(plumber({errorHandler: onError}))
+    .pipe(sass())
+    .pipe(size({ gzip: true, showFiles: true }))
+    .pipe(prefix())
+    .pipe(rename('main.css'))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(reload({stream:true}))
+    .pipe(cssmin())
+    .pipe(size({ gzip: true, showFiles: true }))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "dist/"
+        }
+    });
+});
+
+gulp.task('deploy', function () {
+    return gulp.src('dist/**/*')
+        .pipe(deploy());
+});
+
+gulp.task('js', function() {
+  return gulp.src('js/**/*.js')
+    .pipe(uglify())
+    .pipe(size({ gzip: true, showFiles: true }))
+    .pipe(concat('j.js'))
+    .pipe(gulp.dest('dist/js'))
+    .pipe(reload({stream:true}));
+});
+
+gulp.task('js-watch', ['js'], function (done) {
+    browserSync.reload();
+    done();
+});
+
+gulp.task('scss-lint', function() {
+  gulp.src('scss/**/*.scss')
+    .pipe(cache('scsslint'))
+    .pipe(scsslint());
+});
+
+gulp.task('nunjucks', function() {
+  // Gets .html and .nunjucks files in pages
+  return gulp.src('templates/pages/**/*.+(html|nunjucks)')
+  // Renders template with nunjucks
+  .pipe(nunjucksRender({
+      path: ['templates']
+    }))
+  // output files in app folder
+  .pipe(gulp.dest('templates/compiled'))
+});
+
+gulp.task('minify', ['nunjucks'], function() {
+    return gulp.src('templates/compiled/**/*.html')
+      .pipe(htmlmin({ collapseWhitespace: true }))
+      .pipe(gulp.dest('dist'));
+  });
+
+
+gulp.task('jshint', function() {
+  gulp.src('js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('scss/**/*.scss', ['scss']);
+  gulp.watch('templates/**/*.nunjucks', ['minify']).on("change", reload);
+  gulp.watch('js/*.js', ['jshint', 'js-watch']);
+  gulp.watch('./*.html', ['minify']);
+  gulp.watch('img/*', ['imgmin']);
+});
+
+gulp.task('imgmin', function () {
+    return gulp.src('img/**/*')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('default', ['browser-sync', 'js', 'imgmin', 'minify', 'scss', 'watch']);
